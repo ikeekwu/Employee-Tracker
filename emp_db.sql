8b2c9b2a629f1f245129d1a7b868af6216775102
Ike Ekwueme <cryptoekwu@gmail.com>
2020-08-05 04:34:29 -0500

    db file changes

--- emp_db.sql
+++ emp_db.sql
@@ -5,7 +5,7 @@ CREATE DATABASE emp_db;
 USE emp_db;
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT,
+	id INT AUTO_INCREMENT,
     name VARCHAR(45) NOT NULL,
 
     PRIMARY KEY (id)
@@ -17,7 +17,9 @@ CREATE TABLE emp_role (
     salary DECIMAL(12),
     department_id INT NOT NULL,
 
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+    
 );
 
 CREATE TABLE employee (
@@ -27,7 +29,8 @@ CREATE TABLE employee (
     role_id INT NOT NULL,
     manager_id INT NOT NULL,
     
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES emp_role(id)
 );
 
 INSERT INTO department (name)
@@ -35,4 +38,4 @@ VALUES ("Sales"), ("Finance"),("Engineer"),("Legal") ;
 
 
 
-
+SELECT * FROM department;
